36c1914a1bfc6c7b7893578cb9e81110675df3ff
Bennett Garner <bennett@cubesoftware.com>
2020-02-18 21:00:33 -0500

    Move into folder

--- lambdas/fetch_garnereng_projects_rows.py
+++ lambdas/fetch_garnereng_projects_rows.py
@@ -3,7 +3,7 @@ import boto3
 def lambda_handler(event, context):
     client = boto3.client('dynamodb')
     table_name = 'garnereng_projects'
-    response = client.scan(TableName=table_name, limit=2000)
+    response = client.scan(TableName=table_name, Limit=2000)
 
     return {
         'statusCode': response['ResponseMetadata']['HTTPStatusCode'],
